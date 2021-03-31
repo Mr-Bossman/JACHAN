2931298481bae6e2c4206f9ab7a570ca173b0fbc
Jesse <mr.bossman075@gmail.com>
2021-03-31 17:49:51 -0400

    fixed button

--- bakend.js
+++ bakend.js
@@ -5,31 +5,28 @@ const app = express();
 
 
 app.get('/', (req, res, next) => {
-  res.sendfile('./main.html');
+    res.sendfile('./main.html');
 })
 app.get('/favicon.png', (req, res, next) => {
-  res.sendfile('./favicon.png');
+    res.sendfile('./favicon.png');
 })
 app.get('/assets/fonts/fontawesome5-overrides.min.css', (req, res, next) => {
-  res.sendfile('./assets/fonts/fontawesome5-overrides.min.css');
+    res.sendfile('./assets/fonts/fontawesome5-overrides.min.css');
 })
 app.get('/assets/js/theme.js', (req, res, next) => {
-  res.sendfile('./assets/js/theme.js');
+    res.sendfile('./assets/js/theme.js');
 })
 app.get('/assets/css/untitled.css', (req, res, next) => {
-  res.sendfile('./assets/css/untitled.css');
-})
-app.get('/assets/css/Button-Outlines---Pretty.css', (req, res, next) => {
-  res.sendfile('./assets/css/Button-Outlines---Pretty.css');
+    res.sendfile('./assets/css/untitled.css');
 })
 app.get('/assets/bootstrap/css/bootstrap.min.css', (req, res, next) => {
-  res.sendfile('./assets/bootstrap/css/bootstrap.min.css');
+    res.sendfile('./assets/bootstrap/css/bootstrap.min.css');
 })
 app.get('/main', (req, res, next) => {
-  res.sendfile('./main.txt');
+    res.sendfile('./main.txt');
 })
 app.get('/f1c100s', (req, res, next) => {
-  res.sendfile('./f1c100s.txt');
+    res.sendfile('./f1c100s.txt');
 })
 
 app.listen(80);

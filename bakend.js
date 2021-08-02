9664d283eb016fba7827b5af6882b7fc3ce27bfb
Jesse <mr.bossman075@gmail.com>
2021-08-01 23:08:29 -0400

    center

--- bakend.js
+++ bakend.js
@@ -4,40 +4,29 @@ const app = express();
 const testFolder = './blogs/';
 const fs = require('fs');
 
+fs.readdirSync(testFolder).forEach(file => {
+    app.get('/' + file.split('.')[0], (req, res, next) => {
+        res.sendFile('./blogs/' + file, { root: __dirname });
+    })
 
+});
 app.get('/', (req, res, next) => {
-    res.sendfile('./main.html');
+    res.sendFile('./main.html', { root: __dirname });
 })
 app.get('/favicon.png', (req, res, next) => {
-    res.sendfile('./favicon.png');
+    res.sendFile('./favicon.png', { root: __dirname });
 })
 app.get('/assets/fonts/fontawesome5-overrides.min.css', (req, res, next) => {
-    res.sendfile('./assets/fonts/fontawesome5-overrides.min.css');
+    res.sendFile('./assets/fonts/fontawesome5-overrides.min.css', { root: __dirname });
 })
 app.get('/assets/js/theme.js', (req, res, next) => {
-    res.sendfile('./assets/js/theme.js');
+    res.sendFile('./assets/js/theme.js', { root: __dirname });
 })
 app.get('/assets/css/untitled.css', (req, res, next) => {
-    res.sendfile('./assets/css/untitled.css');
+    res.sendFile('./assets/css/untitled.css', { root: __dirname });
 })
 app.get('/assets/bootstrap/css/bootstrap.min.css', (req, res, next) => {
-    res.sendfile('./assets/bootstrap/css/bootstrap.min.css');
-})
-app.get('/main', (req, res, next) => {
-    res.sendfile('./blogs/main.txt');
-})
-app.get('/f1c100s', (req, res, next) => {
-    res.sendfile('./blogs/f1c100s.txt');
-})
-app.get('/verus', (req, res, next) => {
-    res.sendfile('./blogs/verus.txt');
-})
-
-app.get('/boost', (req, res, next) => {
-    res.sendfile('./blogs/boost.txt');
-})
-app.get('/infINT', (req, res, next) => {
-    res.sendfile('./blogs/infINT.txt');
+    res.sendFile('./assets/bootstrap/css/bootstrap.min.css', { root: __dirname });
 })
 
 
